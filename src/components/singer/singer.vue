dde9669606a31f9bade37cff9d1f3e2ce0c59eee
xudianzhang <469487939@qq.com>
2018-04-08 01:34:14 +0800

    commit my code

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="singer" ref="singer">
+    singer
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
