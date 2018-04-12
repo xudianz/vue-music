392b7eeda84003f7836d352cc79777021d3b7b7d
xudianzhang <xudianzhang@geotmt.com>
2018-04-12 21:04:28 +0800

    抓取歌手详情数据【url失效】

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -8,8 +8,14 @@
 import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
 
 export default {
+  data () {
+    return {
+      songs: []
+    }
+  },
   computed: {
     ...mapGetters([
       'singer'
@@ -21,11 +27,28 @@ export default {
   },
   methods: {
     _getDetail () {
+      // 边界处理 【直接刷新 无数据】
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.list)
+          // console.log(res.data.list)
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
         }
       })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach((item) => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
     }
   }
 }
