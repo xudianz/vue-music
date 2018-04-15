8b45347a95268ab6478f2bf7cd1ba7201c5af841
xudianzhang <469487939@qq.com>
2018-04-16 00:03:11 +0800

    7-1 ===> start

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter"></div>
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length > 0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
-    <scroll :data="songs" class="list" ref="list">
+    <div class="bg-layer" ref="layer"></div>
+    <scroll :data="songs" @scroll="scroll" :probeType="probeType" :listenScroll="listenScroll" class="list" ref="list">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list :songs="songs" @select="selectItem"></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -18,7 +28,13 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import { prefixStyle } from 'common/js/dom'
+import Loading from 'base/loading/loading'
+import { mapActions } from 'vuex'
 
+const RESERVED_HEIGHT = 40 // 滑动到距离顶部的距离40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 export default {
   props: {
     bgImage: {
@@ -34,17 +50,79 @@ export default {
       default: ''
     }
   },
+  data () {
+    return {
+      scrollY: 0
+    }
+  },
   computed: {
     bgStyle () {
       return `background-image:url(${this.bgImage})`
     }
   },
+  created () {
+    this.probeType = 3
+    this.listenScroll = true
+  },
   mounted () {
-    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+    this.imageHeight = this.$refs.bgImage.clientHeight
+    this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+    this.$refs.list.$el.style.top = `${this.imageHeight}px`
+  },
+  methods: {
+    scroll (pos) {
+      this.scrollY = pos.y
+    },
+    back () {
+      this.$router.back()
+    },
+    selectItem (item, index) {
+      this.selectPlay({
+        list: this.songs,
+        index: index
+      })
+    },
+    ...mapActions([
+      'selectPlay'
+    ])
+  },
+  watch: {
+    scrollY (newY) {
+      let translateY = Math.max(this.minTranslateY, newY)
+      let zIndex = 0
+      let scale = 1
+      let blur = 0
+      this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
+      // this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+
+      const perfect = Math.abs(newY / this.imageHeight)
+      if (newY > 0) {
+        scale = 1 + perfect
+        zIndex = 10
+      } else {
+        blur = Math.min(20 * perfect, 20)
+      }
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      // this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
+      if (newY < this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
+      // this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+    }
   },
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   }
 }
 </script>
@@ -130,6 +208,7 @@ export default {
     bottom 0
     width 100%
     background $color-background
+    // overflow hidden
     .song-list-wrapper
       padding 20px 30px
     .loading-container
---
+++ src/store/actions.js
@@ -0,0 +1,9 @@
+import * as types from './mutation-types' // 多次提交mutation 往往要封装一个action
+
+export const selectPlay = function ({commit, state}, {list, index}) {
+  commit(types.SET_SEQUENCE_LIST, list)
+  commit(types.SET_PLAYLIST, list)
+  commit(types.SET_CURRENT_INDEX, index)
+  commit(types.SET_PLAYING_STATE, true)
+  commit(types.SET_FULL_SCREEN, true)
+}
